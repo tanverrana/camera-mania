eaa58f017115ae08969f962811c9d5542366452c
tanverrana <tanversobur@gmail.com>
2022-04-04 14:17:46 +0600

    not found page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>The page you are looking for not available.</h2>
+            <h3>404 </h3>
+            <h1>):</h1>
+        </div>
+    );
+};
+
+export default NotFound;
