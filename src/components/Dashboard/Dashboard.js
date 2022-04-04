64340873908f21e2f38d7ddc2c36a37db99b9d4c
tanverrana <tanversobur@gmail.com>
2022-04-04 20:27:01 +0600

    my line chart add

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MyLineChart from '../MyLineChart/MyLineChart';
 
 const Dashboard = () => {
     return (
         <div>
             <h2>This is Dashboard.</h2>
+            <MyLineChart></MyLineChart>
         </div>
     );
 };
---
+++ src/components/MyLineChart/MyLineChart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyLineChart = () => {
+    return (
+        <div>
+            <h2>My Line Chart</h2>
+        </div>
+    );
+};
+
+export default MyLineChart;
