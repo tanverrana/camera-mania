f10d0f095e20b5ffe1e4b67facc30fd1559fb3db
tanverrana <tanversobur@gmail.com>
2022-04-04 20:44:50 +0600

    line chart added

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import "./MyLineChart.css";
 
 const MyLineChart = () => {
+    const [chart, setChart] = useState([]);
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setChart(data))
+    }, [])
+
     return (
-        <div>
-            <h2>My Line Chart</h2>
+        <div className="line-chart">
+            <h3>Month Wise Sell</h3>
+            <LineChart width={400} height={400} data={chart}>
+                <Line dataKey={"sell"}></Line>
+                <XAxis dataKey={"month"}></XAxis>
+                <YAxis dataKey={"sell"}></YAxis>
+                <Tooltip></Tooltip>
+            </LineChart>
         </div>
     );
 };
