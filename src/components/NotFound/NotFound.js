b633a5be2a5f21a2c01e598f8d0c3dca08e4a24d
tanverrana <tanversobur@gmail.com>
2022-04-04 22:43:04 +0600

    not found page edit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import "./NotFound.css"
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="not-found">
             <h2>The page you are looking for not available.</h2>
-            <h3>404 </h3>
-            <h1>):</h1>
+            <img src="https://img.freepik.com/free-vector/error-404-concept-landing-page_52683-20173.jpg?t=st=1649090297~exp=1649090897~hmac=915ea71443dc0d327987c109c787319c6b38b7693c1e508195553ee7bb0d9b69&w=1060" alt="" />
         </div>
     );
 };
